aa524aeeb4814cfa739c611441a9afe8b64430bc
AnumulaChaitanyaSai <anumulacs@iitbhilai.ac.in>
2023-04-01 19:14:51 +0530

    made a  basic web structure

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import bg from "../assets/icon.png"
+export default function Home(){
+    return(
+        <div className="home_back">
+            <img src={bg} />
+            <h1>Hellowww!</h1>
+        </div>
+    )
+
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import reactLogo from "../assets/logo192.png"
+import './Styles.css'
+
+export default function Navbar(){
+    return(
+    <div className="nav">
+        <h2 className="one">Welcome!</h2>
+        <h2 className="two"> Contact Us</h2>
+    </div>
+    )
+}
