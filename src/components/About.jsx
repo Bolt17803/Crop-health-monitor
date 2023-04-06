03796c33b3007408f24f5c89d7d7d00e5d869c70
HemanthGaddey <gaddeyhemanth@gmail.com>
2023-04-06 16:02:40 +0530

    Changed the UI, added image crop and about page and cropping functionality

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About(){
+    return (
+        <div className="page--about" id="about">
+            <div className="page--about--container">
+                <h1>Made By:</h1>
+                <h2>Gaddey Hemanth Chowdary &</h2>
+                <h2>Anumula Chaitanya Sai</h2>
+            </div>
+        </div>
+    )
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,19 +4,20 @@ import { BrowserRouter, Link } from "react-router-dom"
 import { HashLink } from "react-router-hash-link"
 import Navbar from './Navbar'
 import Crop from "./Crop"
+
 export default function Home(){
     return(
         < BrowserRouter>
-        <div className="home_back">
-            {/* <Navbar /> */}
-            <img src={bg} />
-            <h1>Hellowww!</h1>
-            <HashLink to="#crop" smooth>
-                <button className="hometocrop_button">
-                    <h2>Try it</h2>
-                </button>
-            </HashLink>
-            <Crop />
+        <div className="home_back" id="home">
+            <div className="home--container">
+                <img src={bg} className="home--image"/>
+                <h1 className="home--text">Hellowww!</h1>
+                <HashLink to="#crop" smooth>
+                    <button className="hometocrop--button">
+                    Try it
+                    </button>
+                </HashLink>
+            </div>
         </div>
         </BrowserRouter>
     )
