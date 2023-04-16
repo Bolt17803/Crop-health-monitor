526a3f366fbfea03f75c03aeb892a572e459dee9
AnumulaChaitanyaSai <anumulacs@iitbhilai.ac.in>
2023-04-16 22:40:37 +0530

    new-home_page

---
+++ src/components/Newnavbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import '../Styles.css'
+import { BrowserRouter, Link } from "react-router-dom"
+import { HashLink } from "react-router-hash-link"
+
+export default function Newnavbar(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
