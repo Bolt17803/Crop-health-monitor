661622954cb67108ab7334960d28cd97645986fd
AnumulaChaitanyaSai <anumulacs@iitbhilai.ac.in>
2023-04-17 16:45:36 +0530

    responsive

--- src/components/Newhome.jsx
+++ src/components/Newhome.jsx
@@ -12,9 +12,9 @@ export default function Newhome(){
             <div className="heading">
                 <h1>Crop Health Monitor</h1>
             </div>
-            <div className="Home_content"><h2>Empowering farmers with smart crop health solutions</h2></div>
+            <div className="Home_content"><h2 className="home_text">Empowering farmers with smart crop health solutions</h2></div>
             <img src={green_cicle} className="green_circle" />
-            <div className="nav_buttons"><h3>Analyze plant</h3></div>
+            <div><h3 className="analyze">Analyze plant</h3></div>
             <img src={white1} className="white1" />
             <img src={white2} className="white2" />
             <img src={crop} className="crop" />
