d9ef12628cddb19924fa6bc4069d848f33d11460
AnumulaChaitanyaSai <anumulacs@iitbhilai.ac.in>
2023-04-17 14:27:52 +0530

    added_backend

---
+++ backend/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, File
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+import io
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/upload")
+async def recieveFile(file: bytes = File(...)):
+    image = Image.open(io.BytesIO(file))
+    image.show()
+    print(file)
+    return {"uploadStatus":"Complete"}
