2831e2a3f4321629154f80ae7e6092822ad35952
chaitanyasai <anumulacs@iitbhilai.ac.in>
2023-05-29 12:39:54 -0700

    changed ststic.yml folder

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Crop-health-monitor/",
-  build: {
-    outDir: 'dist',
-    assetsDir: '',
-  },
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-      '@css': '/src/Styles.css',
-    },
-  },
+  base: "/Crop-health-monitor/"
 })
