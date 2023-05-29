22d285d1554b6e47cf9787e26d2ffb761a2c5e2d
chaitanyasai <anumulacs@iitbhilai.ac.in>
2023-05-29 11:43:02 -0700

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Crop-health-monitor/",
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@css': '/src/Styles.css',
+    },
+  },
 })
