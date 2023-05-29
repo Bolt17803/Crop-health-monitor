d8b273280be970b62799b55ad1aa9d94bf91a0d7
HemanthGaddey <gaddeyhemanth@gmail.com>
2023-05-29 11:11:06 +0530

    Added backend API for classification and front end crop page changes with additional UI, first completed protoype version of the page: v1

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import io
+from enum import Enum
+from pydantic import BaseModel
+import uvicorn
+from typing import List
+from segment_anything import sam_model_registry, SamPredictor
+import segmentation_api as segmentor
+import classification_api as classifier
+import json
+import numpy as np
+import cv2
+import base64
+
+pil_image = None
 
 app = FastAPI()
 
@@ -18,19 +31,61 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class Input(BaseModel) :
+    x1: int
+    y1: int
+    x2: int
+    y2: int
+
+@app.post("/item/")
+async def coord(coord: Input):
+    return coord
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.post("/upload")
 async def recieveFile(file: bytes = File(...),data: str = Form(...)):
-    image = Image.open(io.BytesIO(file))
-    #image.show()
-    #print(file)
-    print(data)
-    return {"uploadStatus":"Complete"}
+    #image = Image.open(io.BytesIO(file))
+    #encoded_image = base64.b64encode(file).decode("utf-8")
+    # nparr = np.fromstring(base64.b64decode(encoded_image), np.uint8)
+    # img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    print("received")
+    data = json.loads(data)
+    coords = [data["x1"],data["y1"],data["x2"],data["y2"]]
+    print(coords)
+    img_b64s,img_cv2 = segmentor.getMask64(file,coords)
+    color_coverted = cv2.cvtColor(img_cv2, cv2.COLOR_BGR2RGB)
+    pil_image = Image.fromarray(color_coverted)
+    pil_image = pil_image.crop((*coords,))
+    pil_image = pil_image.resize((256,256))
+    pil_image.save(f"./apple_black_rot_segmented/{coords}.png")
+    #print(img_b64s)
+    return {"uploadStatus":"Complete","result":"data:image/jpeg;base64,"+img_b64s}#classifier.predict(image)}
+    
 
 @app.post("/upload_selection")
-async def recieveAnotherFile(data: str = Form(...)):
+async def recieveAnotherFile(file: bytes = File(...),data: str = Form(...)):
     print(data)
-    return {"uploadStatus":"Complete"}
+    img = Image.open(io.BytesIO(file))
+    result=classifier.predict(img)
+    print("result:",result)
+    return {"uploadStatus":"Complete","result":result}
+
+@app.get("/")
+def root():
+    return {"message": "Hello"}
+
+# @app.post("/upload")
+# async def recieveFile(file: bytes = File(...)):
+#     image = Image.open(io.BytesIO(file))
+#     image.show()
+#     print(file)
+#     return {"uploadStatus":"Complete"}
+
+
+# @app.post('/analyze')
+# async def analyze(image: bytes = File(...), coord: List[int] = Form(...), points: List[int] = Form(...), labels: List[int] = Form(...)):
+#     img_bytes = segmentor.getMask(image,coord,points,labels)
+#     return {"result": img_bytes}
