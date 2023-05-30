6ff6e6bd25c43713ddb1a58436fad7a9a89077ab
chaitanyasai <anumulacs@iitbhilai.ac.in>
2023-05-29 23:26:23 -0700

    Updates

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About(){
+    return (
+        <div className="page--about" id="about">
+            <div className="page--about--container">
+                <h1>Made By:</h1>
+                <h2>Gaddey Hemanth Chowdary & Anumula Chaitanya Sai</h2>
+                {/* <h2>Anumula Chaitanya Sai</h2> */}
+                <p>@ idhi maa adda nuvveedraa naa ***</p>
+            </div>
+        </div>
+    )
+}
