c5999faa9817a9e8533431e1527f9e4a8d12eb34
HemanthGaddey <gaddeyhemanth@gmail.com>
2023-05-30 22:42:50 +0530

    Added the results page, added navigation router to it and changed the results from single result to top 3 results with probabilities

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,27 @@ import Newhome from "./components/Newhome";
 import About from "./components/About";
 import Results from "./components/Results";
 import './Styles.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [result,setResult] = useState(null);
+
+  const updateResult = (newResult) => {
+    setResult(newResult);
+  };
+
+  useEffect(()=>{
+    console.log("UPDATED RESULTS:"+result);
+  },[result]);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/Crop-health-monitor" element={<Newhome />} />
-        <Route exact path="/Crop-health-monitor/Analyze" element={<Crop />} />
+        <Route exact path="/Crop-health-monitor/Analyze" element={<Crop setResultFunction={updateResult}/>} />
         <Route exact path="/Crop-health-monitor/About" element={<About />} />
-        <Route exact path="/Crop-health-monitor/Results" element={<Results />} />
+        <Route exact path="/Crop-health-monitor/Results" element={<Results results={result}/>} />
       </Routes>
     </BrowserRouter>
   )
